2f74f22ecb4df107efb4fdd24a30a010bb331850
Benjamin Wegener <me@inselmensch.de>
2023-02-22 17:07:23 +0000

    qa: code cleanup

--- App.js
+++ App.js
@@ -1,54 +1,53 @@
 #! /usr/bin/env node
-const yargs = require('yargs').argv
+const yargs = require('yargs').argv;
 const path = require('path');
-const locale = process.env.LC_ALL || process.env.LC_MESSAGES || process.env.LANG || process.env.LANGUAGE || 'en'
+const locale = process.env.LC_ALL || process.env.LC_MESSAGES || process.env.LANG || process.env.LANGUAGE || 'en';
 const readline = require('readline');
 
 const notifier = require('node-notifier');
 
-const Storage = require('./Storage')
+const Storage = require('./Storage');
 const Trackerino = require('trackerino-core');
 
-if(yargs.version || yargs.v) {
-	let { version, name } = require('./package.json')
-	console.log(version)
-	process.exit(0)
+if (yargs.version || yargs.v) {
+	let { version, name } = require('./package.json');
+	console.log(version);
+	process.exit(0);
 }
 
-let today = null
-if(yargs.today) {
+let today = null;
+if (yargs.today) {
 	try {
-		today = new Date(Date.parse(yargs.today))
-	}
-	catch (err) {
-		console.log(err)
-		process.exit(1)
+		today = new Date(Date.parse(yargs.today));
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
 	}
 }
 
 const $storage = new Storage({
-	date: today
-})
+	date: today,
+});
 
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 	completer: function () {
-		console.log('TODO: something')
-	}
-})
+		console.log('TODO: something');
+	},
+});
 
 const trackerino = new Trackerino({
 	storage: $storage,
 	date: today,
 	onBeforeAnswer: (_input, _interface) => {
-		if(_input == '/quit') {
-			_interface.onClose()
+		if (_input == '/quit') {
+			_interface.onClose();
 		}
 	},
 	onClose: () => {
-		console.log("\n👋 BYE 😊\n")
-		process.exit(0)
+		console.log('\n👋 BYE 😊\n');
+		process.exit(0);
 	},
 	onTick: function (_interface) {},
 	onNotify: function (options) {
@@ -56,23 +55,27 @@ const trackerino = new Trackerino({
 			title: options.title || 'Trackerino CLI',
 			message: options.message || 'No Message (yet?)!',
 			sound: options.sound || false,
-			wait: options.wait || false
+			wait: options.wait || false,
 		});
 	},
-	onAsk: (q, handle, _interface) => rl.question(q, async (_input) => {
-		let output = await handle(_input)
-		// console.log(output)
-		_interface.ask()
-	}),
-	onError: text => console.error('ERR', text),
+	onAsk: (q, handle, _interface) =>
+		rl.question(q, async (_input) => {
+			let output = await handle(_input);
+			_interface.ask();
+
+			if (output && output.data && typeof output.data == 'string') {
+				_interface.say(output.data);
+			}
+		}),
+	onError: (text) => console.error('ERR', text),
 	onOutput: (text, cmd) => {
-		console.log(text)
-	}
-})
+		console.log(text);
+	},
+});
 
-let _AppInterval = setInterval(trackerino.save.bind(trackerino), 10000)
+let _AppInterval = setInterval(trackerino.save.bind(trackerino), 10000);
 
-rl.on("close", () => {
-	clearInterval(_AppInterval)
-	trackerino.onClose()
-})
+rl.on('close', () => {
+	clearInterval(_AppInterval);
+	trackerino.onClose();
+});
